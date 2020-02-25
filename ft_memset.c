f532e90e7e9f82db195203ee85b0d590cb57029e
Iuliia Kuklina <ikuklina@e1z1r6p36.42.us.org>
2020-02-25 14:47:31 -0800

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memset.c                                           :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 09:15:30 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/02/21 14:51:37 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/02/25 14:47:25 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*it's a copy of the memset function in the string.h library*/
-
-void    *memset(void *str, int c, size_t len)
+void    *ft_memset(void *str, int c, size_t len)
 {
-    size_t i;
+    unsigned char   *ptr;
+    size_t          i;
     
     i = 0;
+    ptr = (unsigned char*)str;
     while (i < len)
     {
-        str[i] = c;
-        i++;   
+        ptr[i] = (unsigned char)c;
+        i++;
     }
     return (str);
 }
