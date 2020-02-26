77ed3886a36b59fdd5dcb4dcd1873d046cbfac89
Iuliia Kuklina <ikuklina@e1z1r6p36.42.us.org>
2020-02-25 19:52:05 -0800

    norm

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 10:30:01 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/02/25 19:49:09 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/02/25 19:51:44 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_bzero(void *s, size_t n)
+void ft_bzero(void *s, size_t n)
 {
     ft_memset(s, '\0', n);
 }
