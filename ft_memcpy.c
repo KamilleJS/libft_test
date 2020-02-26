b3f5d25ca370b473b10dfa4b5365d39dd67e02ee
Iuliia Kuklina <ikuklina@e1z1r6p36.42.us.org>
2020-02-25 16:02:20 -0800

    memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/25 15:25:10 by ikuklina          #+#    #+#             */
+/*   Updated: 2020/02/25 16:02:12 by ikuklina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memcpy(void *str1, const void *str2, size_t n)
+{
+    size_t          i;
+    unsigned char   *dst;
+    unsigned char   *src;
+
+    i = 0;
+    dst = (unsigned char *)str1;
+    src = (unsigned char *)str2;
+    while (i < n)
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    return (str1);
+}
