bd4f07dc60c77588484ab3f8cd44a7a4a7921f64
Iuliia Kuklina <ikuklina@e1z1r6p36.42.us.org>
2020-02-25 20:03:23 -0800

    norm

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 09:15:30 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/02/25 19:35:33 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/02/25 20:03:09 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void    *ft_memset(void *str, int c, size_t len)
 {
     unsigned char   *ptr;
-    size_t          i;
-    
+    size_t  i;
+
     i = 0;
     ptr = (unsigned char*)str;
     while (i < len)
