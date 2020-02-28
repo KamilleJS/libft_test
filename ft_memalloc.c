e55a0d40987bf29240d8f73e9fb47d3f9cffb907
Yuliya <ikuklina@student.42.us.org>
2020-02-27 18:58:58 -0800

    malloc

---
+++ ft_memalloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/27 18:18:49 by ikuklina          #+#    #+#             */
+/*   Updated: 2020/02/27 18:57:45 by ikuklina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** allocates and returns a memory area
+*/
+
+void	*ft_memalloc(size_t size)
+{
+	char	*str;
+
+	str = (char*)malloc(sizeof(*str) * size);
+	if (str == NULL)
+		return (NULL);
+	ft_bzero(str, size);
+	return (str);
+}
