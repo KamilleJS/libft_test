3c09a25a72119c9efeb18e0d216176704f7589e6
Yuliya <ikuklina@student.42.us.org>
2020-03-02 18:23:01 -0800

    comment

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,16 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putenl_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 16:38:30 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/02/28 16:43:14 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/02 18:22:45 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** outputs the string s to the file descriptor fd followed by a new line
+*/
 
 void	ft_putendl_fd(char const *s, int fd)
 {
