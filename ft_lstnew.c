8d4818a129242a60da80e49ecf9a87c48fdd5513
Yuliya <ikuklina@student.42.us.org>
2020-03-02 17:15:33 -0800

    lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,13 +6,34 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 16:45:55 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/02/28 16:46:26 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/02 17:15:18 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+** allocates and returns a link
+*/
+
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
-	
+	t_list	*lstnew;
+
+	if (!(lstnew = (t_list *)malloc(sizeof(t_list) * content_size)))
+		return (NULL);
+	if (!content)
+	{
+		lstnew->content = NULL;
+		lstnew->content_size = 0;
+	}
+	else
+	{
+		if (!(lstnew->content = (void *)malloc(content_size)))
+			return (NULL);
+		ft_memcpy(lstnew->content, content, content_size);
+		lstnew->content_size = content_size;
+	}
+	lstnew->next = NULL;
+	return (lstnew);
 }
