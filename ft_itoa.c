9d4e0d576c1f5465de297aee2e69311a109a1f0d
Yuliya <ikuklina@student.42.us.org>
2020-03-02 18:19:46 -0800

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,41 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 14:04:28 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/02/28 16:22:22 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/02 18:19:31 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+** allocate and returns a string ending with ’\0’ representing the integer n
+** given as argument
+*/
+
 char	*ft_itoa(int n)
 {
-	gvgvgb
+	char	*str;
+	int		i;
+	int		j;
+	int		num;
+
+	i = 0;
+	if (n < 0)
+		i = 1;
+	num = ft_countnum(n);
+	str = ft_strnew(num + i);
+	if (str == NULL)
+		return (NULL);
+	if (i)
+		str[0] = '-';
+	j = num + i - 1;
+	while (j >= i)
+	{
+		if (i)
+			str[j--] = (n % 10 * -1) + '0';
+		else
+			str[j--] = n % 10 + '0';
+		n = n / 10;
+	}
+	return (str);
 }
