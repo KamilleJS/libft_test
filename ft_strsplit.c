32c369eb6576231bc20ab4c3b5abd6cfbafe0cce
Yuliya <ikuklina@student.42.us.org>
2020-03-05 15:16:07 -0800

    not yet

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,11 +6,16 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 13:03:50 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/02/28 13:04:27 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/05 14:10:07 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** allocates and returns an array of strings (all ending with ’\0’, including the array itself)
+** obtained by spliting s using the character c as a delimiter
+*/
 
 char	**ft_strsplit(char const *s, char c)
 {
