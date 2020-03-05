1849a84544989537972cd7165767d5cfc3fee542
Yuliya <ikuklina@student.42.us.org>
2020-03-05 15:36:13 -0800

    strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/05 15:34:15 by ikuklina          #+#    #+#             */
+/*   Updated: 2020/03/05 15:36:00 by ikuklina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** allocates and returns a copy of the string given as argument without
+** whitespaces at the beginning or at the end of the string
+*/
+
+char	*ft_strtrim(char const *s)
+{
+
+}
