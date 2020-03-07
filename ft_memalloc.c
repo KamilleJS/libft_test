0bd760171d1d37aec49732f8c3963f1156d542cb
Yuliya <ikuklina@student.42.us.org>
2020-03-06 22:35:50 -0800

    changes

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 18:18:49 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/02/27 18:57:45 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/06 22:25:52 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	*ft_memalloc(size_t size)
 {
 	char	*str;
 
-	str = (char*)malloc(sizeof(*str) * size);
+	str = (char*)malloc(sizeof(char) * size);
 	if (str == NULL)
 		return (NULL);
 	ft_bzero(str, size);
-	return (str);
+	return ((void*)str);
 }
--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 13:03:50 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/03/05 14:10:07 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/06 18:38:18 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,34 @@
 ** obtained by spliting s using the character c as a delimiter
 */
 
-char	**ft_strsplit(char const *s, char c)
+int	ft_countwords(char const *str, char c)
 {
-	
+	int count;
+	int	i;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (str[i] == c)
+			i++;
+		if (str[i] && str[i] != c)
+			count++;
+		while (str[i] && str[i] != c)
+			i++;
+	}
+	return (count);
 }
+
+char	**ft_strsplit(char const *str, char c)
+{
+	int	i;
+	int	words;
+
+	i = 0;
+	words = ft_countwords(str, c);
+	if (!(tab = (char**)malloc(sizeof(char*) * (words + 1))))
+		return (NULL);
+
+}
+
