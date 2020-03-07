a7b9ed6e911bcae0a59cc9a50da78402eff04da0
Yuliya <ikuklina@student.42.us.org>
2020-03-07 10:46:14 -0800

    strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 13:03:50 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/03/06 18:38:18 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/07 10:45:52 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,30 @@
 ** obtained by spliting s using the character c as a delimiter
 */
 
-int	ft_countwords(char const *str, char c)
-{
-	int count;
-	int	i;
-
-	i = 0;
-	count = 0;
-	while (str[i])
-	{
-		while (str[i] == c)
-			i++;
-		if (str[i] && str[i] != c)
-			count++;
-		while (str[i] && str[i] != c)
-			i++;
-	}
-	return (count);
-}
-
 char	**ft_strsplit(char const *str, char c)
 {
-	int	i;
-	int	words;
+	char	**s;
+	int		num;
+	int		i;
+	int		j;
 
-	i = 0;
-	words = ft_countwords(str, c);
-	if (!(tab = (char**)malloc(sizeof(char*) * (words + 1))))
+	if (str == NULL)
 		return (NULL);
-
+	num = ft_countwords(str, c);
+	s = (char**)malloc(sizeof(char*) * (num + 1));
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < num)
+	{
+		while (str[j] && str[j] == c)
+			j++;
+		s[i] = ft_strsub(str, j, ft_length(str + j, c));
+		while (str[j] && str[j] != c)
+			j++;
+		i++;
+	}
+	s[i] = NULL;
+	return (s);
 }
-
