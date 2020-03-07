af7928edf97ef6a0509e2a76621118ba194367ad
Yuliya <ikuklina@student.42.us.org>
2020-03-07 10:54:51 -0800

    changes

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,14 +6,15 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 13:03:50 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/03/07 10:45:52 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/07 10:54:36 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** allocates and returns an array of strings (all ending with ’\0’, including the array itself)
+** allocates and returns an array of strings (all ending with ’\0’,
+** including the array itself)
 ** obtained by spliting s using the character c as a delimiter
 */
 
