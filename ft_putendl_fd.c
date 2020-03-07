39afb7ec9a46b21d071e279c8287ecb496e26b4f
Yuliya <ikuklina@student.42.us.org>
2020-03-07 10:24:13 -0800

    putendlfd

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 16:38:30 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/03/02 18:22:45 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/07 10:23:42 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,13 @@
 
 void	ft_putendl_fd(char const *s, int fd)
 {
-	ft_putstr_fd(s, fd);
-	ft_putchar_fd('\n', fd);
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
 }
