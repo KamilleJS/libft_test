50b6d25b3310c7a6e1d26817dccb6357fbfe060a
Yuliya <ikuklina@student.42.us.org>
2020-03-07 10:45:22 -0800

    length

---
+++ ft_length.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_length.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/07 10:43:30 by ikuklina          #+#    #+#             */
+/*   Updated: 2020/03/07 10:45:06 by ikuklina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** find length of words
+*/
+
+int	length(const char *str, char c)
+{
+	int len;
+
+	len = 0;
+	while (str[len] && str[len] != c)
+		len++;
+	return (len);
+}
