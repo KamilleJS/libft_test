27c8c81fe49f111c9bc1aae6bce35ca13f363086
Yuliya <ikuklina@student.42.us.org>
2020-03-07 17:31:41 -0800

    changes

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 09:11:20 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/03/07 17:03:29 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/07 17:31:30 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,9 @@ int	ft_strequ(char const *s1, char const *s2)
 {
 	if (!s1 || !s2)
 		return (0);
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-			return (0);
-		s1++;
-		s2++;
-	}
-	return (1);
+	if (ft_strlen(s1) != ft_strlen(s2))
+		return (0);
+	if (ft_strcmp(s1, s2) == 0)
+		return (1);
+	return (0);
 }
