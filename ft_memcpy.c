c91bdf0d7e5e4cc36c8cd82d09dc31e9184f08b1
Yuliya <ikuklina@student.42.us.org>
2020-03-08 13:13:43 -0700

    changes

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 15:25:10 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/02/27 09:50:57 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/08 13:13:23 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ void	*ft_memcpy(void *str1, const void *str2, size_t len)
 	i = 0;
 	dst = (unsigned char *)str1;
 	src = (unsigned char *)str2;
+	if (dst == NULL && src == NULL && len)
+		return (NULL);
 	while (i < len)
 	{
 		dst[i] = src[i];
