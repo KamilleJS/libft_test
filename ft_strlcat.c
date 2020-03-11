4895c55ad1b3f0af9516dac339d4101e7ceae834
Yuliya <ikuklina@student.42.us.org>
2020-03-11 09:17:34 -0700

    changes

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 10:38:08 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/03/02 18:35:44 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/11 09:16:42 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,25 @@
 ** size-bounded string copying and concatinating
 */
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dstlen;
+	size_t	j;
 	size_t	srclen;
-	size_t	i;
-	int		j;
 
-	i = 0;
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
 	j = 0;
-	dstlen = 0;
-	srclen = 0;
-	while (dst[i] != '\0')
+	if (dstsize <= dstlen)
+		srclen += dstsize;
+	else
+		srclen += dstlen;
+	while (src[j] != '\0' && dstlen + 1 < dstsize)
 	{
-		i++;
+		dst[dstlen] = src[j];
 		dstlen++;
+		j++;
 	}
-	while (src[srclen] != '\0')
-		srclen++;
-	if (dstlen + 1 > size)
-		return (srclen + size);
-	if (size > dstlen + 1)
-	{
-		while (i < size - 1)
-			*(dst + i++) = *(src + j++);
-		*(dst + i) = '\0';
-	}
-	return (dstlen + srclen);
+	dst[dstlen] = '\0';
+	return (srclen);
 }
