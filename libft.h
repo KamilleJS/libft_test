b06051a55eccba93863d91d503c5be57f886f1a7
Yuliya <ikuklina@student.42.us.org>
2020-03-11 10:21:46 -0700

    c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 15:53:14 by boyola            #+#    #+#             */
-/*   Updated: 2020/03/11 10:15:22 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/11 10:21:39 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,5 +92,6 @@ float				ft_get_max(float a, float b);
 float				ft_get_min(float a, float b);
 int					ft_list_size(t_list *begin_list);
 int					ft_count_symb_char(char *s, char c);
+int					ft_countwords(char const *str, char c);
 
 #endif
