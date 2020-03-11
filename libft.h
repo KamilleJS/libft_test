992ad5368839bec9fca0af359ba73235dc104e38
Yuliya <ikuklina@student.42.us.org>
2020-03-11 14:04:37 -0700

    c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 15:53:14 by boyola            #+#    #+#             */
-/*   Updated: 2020/03/11 14:01:31 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/11 14:03:53 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,5 +94,6 @@ int					ft_list_size(t_list *begin_list);
 int					ft_count_symb_char(char *s, char c);
 int					ft_countnum(int n);
 int					ft_countwords(char const *str, char c);
+int					ft_getpositiv(int num);
 
 #endif
