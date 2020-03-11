204659ef9dc7538b8626db7ae6d8608664b1c37b
Yuliya <ikuklina@student.42.us.org>
2020-03-11 10:16:34 -0700

    tolower

--- ft_tolower.c
+++ ft_tolower.c
@@ -3,18 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: boyola <boyola@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/19 15:37:27 by boyola            #+#    #+#             */
-/*   Updated: 2020/03/07 19:59:15 by boyola           ###   ########.fr       */
+/*   Created: 2020/02/20 12:06:01 by ikuklina          #+#    #+#             */
+/*   Updated: 2020/03/06 12:58:25 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_tolower(int c)
+/*
+** upper case to lower case letter conversion
+*/
+
+int	ft_tolower(int str)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (c + 32);
-	return (c);
+	if (str >= 'A' && str <= 'Z')
+		return (str - 'A' + 'a');
+	return (str);
 }
