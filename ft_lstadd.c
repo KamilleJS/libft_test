4931b3dcdce99bcfceb19343d30ea3bb68075497
Yuliya <ikuklina@student.42.us.org>
2020-03-11 14:12:50 -0700

    lstadd

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -3,21 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: boyola <boyola@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/28 16:49:42 by boyola            #+#    #+#             */
-/*   Updated: 2020/02/28 16:51:39 by boyola           ###   ########.fr       */
+/*   Created: 2020/03/02 17:27:41 by ikuklina          #+#    #+#             */
+/*   Updated: 2020/03/02 17:29:09 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** Adds the element NEW at the beginning of the list.
+** adds the element new at the beginning of the list
 */
 
 void	ft_lstadd(t_list **alst, t_list *new)
 {
-	new->next = *alst;
+	if (alst && *alst && new)
+		new->next = *alst;
 	*alst = new;
 }
