cc425a0a0b2a8a90708574ae91a89f2d015c1787
Yuliya <ikuklina@student.42.us.org>
2020-03-11 14:09:49 -0700

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,55 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: boyola <boyola@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/07 15:23:23 by boyola            #+#    #+#             */
-/*   Updated: 2020/03/07 15:23:27 by boyola           ###   ########.fr       */
+/*   Created: 2020/02/28 14:04:28 by ikuklina          #+#    #+#             */
+/*   Updated: 2020/03/02 18:19:31 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** Allocate (with malloc(3)) and returns a “fresh” string ending with ’\0’
-** representing the integer n given as argument. Negative numbers
-** must be supported. If the allocation fails, the function returns NULL.
+** allocate and returns a string ending with ’\0’ representing the integer n
+** given as argument
 */
 
-static void	itoa_isnegative(int *n, int *negative)
+char	*ft_itoa(int n)
 {
-	if (*n < 0)
-	{
-		*n *= -1;
-		*negative = 1;
-	}
-}
+	char	*str;
+	int		i;
+	int		j;
+	int		num;
 
-char		*ft_itoa(int n)
-{
-	int		temp;
-	int		len;
-	int		negative;
-	char	*string;
-
-	if (n == -2147483648)
-		return (ft_strdup("-2147483648"));
-	temp = n;
-	len = 2;
-	negative = 0;
-	itoa_isnegative(&n, &negative);
-	while (temp /= 10)
-		len++;
-	len += negative;
-	if ((string = (char*)malloc(sizeof(char) * len)) == NULL)
+	i = 0;
+	if (n < 0)
+		i = 1;
+	num = ft_countnum(n);
+	str = ft_strnew(num + i);
+	if (str == NULL)
 		return (NULL);
-	string[--len] = '\0';
-	while (len--)
+	if (i)
+		str[0] = '-';
+	j = num + i - 1;
+	while (j >= i)
 	{
-		string[len] = n % 10 + '0';
+		if (i)
+			str[j--] = (n % 10 * -1) + '0';
+		else
+			str[j--] = n % 10 + '0';
 		n = n / 10;
 	}
-	if (negative)
-		string[0] = '-';
-	return (string);
+	return (str);
 }
