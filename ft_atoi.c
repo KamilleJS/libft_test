6d9fc01916782a87923f53681a8a02af57a4686a
Yuliya <ikuklina@student.42.us.org>
2020-03-10 21:37:24 -0700

    changes

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 09:44:24 by ikuklina          #+#    #+#             */
-/*   Updated: 2020/03/10 21:34:43 by ikuklina         ###   ########.fr       */
+/*   Updated: 2020/03/10 21:37:14 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,24 @@
 ** convert ASCII string to integer
 */
 
-int	ft_atoi(const char *str)
+int		ft_atoi(const char *str)
 {
-	int min;
-	int res;
+	int	result;
+	int	negative;
 
-	min = 1;
-	res = 0;
-	while (*str == ' ' || *str == '\t' || *str == '\n'
-			|| *str == '\r' || *str == '\v' || *str == '\f')
-		str++;
+	negative = 1;
+	result = 0;
+	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' ||
+			*str == '\v' || *str == '\f' || *str == '\r'))
+		++str;
+	if (*str == '-')
+		negative = -1;
 	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			min = -1;
-		str++;
-	}
+		++str;
 	while (*str && *str >= '0' && *str <= '9')
 	{
-		res = res * 10 + *str - '0';
-		str++;
+		result = result * 10 + (*str - 48);
+		++str;
 	}
-	if (res > 2147483647)
-		return (2147483647);
-	if (res < -2147483648)
-		return (-2147483648);
-	return (res * min);
+	return (result * negative);
 }
