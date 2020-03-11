947638e82e58d2b00941668e9bb7d34c7d4ada21
Yuliya <ikuklina@student.42.us.org>
2020-03-11 09:54:14 -0700

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,39 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: boyola <boyola@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ikuklina <ikuklina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/18 20:05:58 by boyola            #+#    #+#             */
-/*   Updated: 2020/03/06 15:47:39 by boyola           ###   ########.fr       */
+/*   Created: 2020/02/19 09:44:24 by ikuklina          #+#    #+#             */
+/*   Updated: 2020/03/10 21:37:14 by ikuklina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** Convert ASCII string to integer.
+** convert ASCII string to integer
 */
 
 int		ft_atoi(const char *str)
 {
-	int sign;
-	int result;
+	int	result;
+	int	negative;
 
-	sign = 1;
+	negative = 1;
 	result = 0;
-	while (*str == ' ' || *str == '\t' || *str == '\n'
-			|| *str == '\r' || *str == '\v' || *str == '\f')
-		str++;
+	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' ||
+			*str == '\v' || *str == '\f' || *str == '\r'))
+		++str;
+	if (*str == '-')
+		negative = -1;
 	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign = -1;
-		str++;
-	}
+		++str;
 	while (*str && *str >= '0' && *str <= '9')
 	{
-		result = result * 10 + *str - '0';
-		str++;
+		result = result * 10 + (*str - 48);
+		++str;
 	}
-	return (result * sign);
+	return (result * negative);
 }
